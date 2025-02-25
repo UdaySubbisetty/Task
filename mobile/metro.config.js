6bce38249349c304619adec6fef2f0871d1a0e7a
Udaya bhaskar Subbisetty <30430465+UdaySubbisetty@users.noreply.github.com>
2025-02-26 01:24:21 +0400

    Updated

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,4 +1,4 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
  * Metro configuration
@@ -13,37 +13,16 @@ const path = require("path");
 const extraNodeModules = {
   common: path.resolve(__dirname, "../common"),
   '@babel/runtime': path.resolve(__dirname, 'node_modules/@babel/runtime'),
-  react: path.resolve(__dirname, 'node_modules/react'),  // ✅ Fix for React resolution
-      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'), // ✅ For React DOM resolution if needed
-      'node_modules': path.resolve(__dirname, 'node_modules'),
+  react: path.resolve(__dirname, 'node_modules/react'),
+  'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+  'node_modules': path.resolve(__dirname, 'node_modules'),
 };
 
-const config = {resolver:{
-  extraNodeModules
-},
-watchFolders: [path.resolve(__dirname, "../common"),path.resolve(__dirname, 'node_modules')], 
+const config = {
+  resolver: {
+    extraNodeModules
+  },
+  watchFolders: [path.resolve(__dirname, "../common"), path.resolve(__dirname, 'node_modules')],
 };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
-
-
-
-// const path = require("path");
-
-// const extraNodeModules = {
-//   common: path.resolve(__dirname, "../common"),
-//   '@babel/runtime': path.resolve(__dirname, 'node_modules/@babel/runtime'),
-//   react: path.resolve(__dirname, 'node_modules/react'),  // ✅ Fix for React resolution
-//       'react-dom': path.resolve(__dirname, 'node_modules/react-dom'), // ✅ For React DOM resolution if needed
-//       'node_modules': path.resolve(__dirname, 'node_modules'),
-// };
-
-// module.exports = {
-//   resolver: {
-//     extraNodeModules,
-//     assetExts: [
-//       'png', 'jpg', 'jpeg', 'bmp', 'gif', 'webp', 'svg',  // Add other image formats if needed
-//     ],
-//   },
-//   watchFolders: [path.resolve(__dirname, "../common"),path.resolve(__dirname, 'node_modules')], // ✅ Allow Metro to watch common folder
-// };
